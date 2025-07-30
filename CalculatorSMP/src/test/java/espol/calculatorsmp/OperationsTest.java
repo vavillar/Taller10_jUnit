0c2f2938dab9334bda1c7e83f511562d813d56e6
Victor <vavillar@espol.edu.ec>
2025-07-30 16:33:34 -0500

    Upload test

---
+++ CalculatorSMP/src/test/java/espol/calculatorsmp/OperationsTest.java
@@ -0,0 +1,62 @@
+package espol.calculatorsmp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CltControl
+ */
+public class OperationsTest {
+    
+    public OperationsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of MakeFormula method, of class Operations.
+     */
+    @Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "";
+        String result = Operations.MakeFormula();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Solve method, of class Operations.
+     */
+    @Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
